dbf322d18d9e1a46b6a403c1a6c2c1f81fa5ea76
harshsaini2375 <harshsaini2375@gmail.com>
2024-11-18 21:51:31 +0530

    nextauth

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+// this is footer
 const Footer = () => {
   return (
     <div className='border border-black flex justify-center font-bold py-2  md:p-5 absolute bottom-0 w-screen bg-slate-950 text-white'>
